32cb2fb70fe81df58e6ed9bd703bf2b89a991416
Vision <geoffrey.kuhl@gmail.com>
2015-01-27 00:46:31 -0500

    Added Comments
    Added comments and cleaned up some of the variable names.
    Changed krustCore.test() to krustCore.console()

--- oxide/plugins/krust/krust.js
+++ oxide/plugins/krust/krust.js
@@ -1,35 +1,83 @@
+/**
+ * Krust
+ *  - In Development -
+ * Version 0.0.1
+ * Created by @VisionMise on GitHub
+ * Revised 2015-01-25
+ *
+ * Issues:  https://github.com/VisionMise/Krust/issues
+ * Wiki:    https://github.com/VisionMise/Krust/wiki
+ */
 
+
+/**
+ * Krust Core Object
+ */
 var krustCore = function() {
 
+
+    /**
+     * Initialize krust
+     */
     this.init           = function() {
         print("Server is Krusty");
         return this;
     };
 
-    this.test           = function(msg) {
+
+    /**
+     * Print to Console
+     */
+    this.console        = function(msg) {
         print(msg);
         return this;
     };
 
+
+    /**
+     * Initialize when set to a new object
+     */
     return this.init();
 };
 
+
+/**
+ * GLOBAL core object
+ */
 core  = new krustCore();
 
+
+/**
+ * Main Oxide Plugin Object
+ */
 var krust = {
+
+    /**
+     * Meta
+     */
     Title : "krust",
-    Author : "Vision",
+    Author : "VisionMise",
     Version : V(0, 0, 1),
     HasConfig : true,
 
+
+    /**
+     * Server Initialization Done
+     */
     OnServerInitialized : function () {
 
-        command.AddChatCommand("krust", this.Plugin, "test");
+        //Bind 'krust_test' Chat Command to this.krust_test()
+        command.AddChatCommand("krust_test", this.Plugin, "krust_test");
     },
 
-    test : function() {
-        //krust.test("hello");
-        core.test("Hello Krust");
+
+    /**
+     * Local Callback
+     */
+    krust_test : function() {
+        //Invoke Core Method
+        //test
+        core.console("Test From Callback");
     }
-    
+
 };
